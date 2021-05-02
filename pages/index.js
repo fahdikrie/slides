3f87b17d543b71e11fc9078c2db83863b10e7fae
fahdikrie <fahdiaf@gmail.com>
2021-05-02 14:06:00 +0700

    fix: change redirect url on anchor tag

--- pages/index.js
+++ pages/index.js
@@ -22,37 +22,9 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="/slides/1-hooks.html" className={styles.card}>
+          <a href="/hooks.html" className={styles.card}>
             <h2>Hooks &rarr;</h2>
             <p>Learn about hooks in an interactive course with reveal.js!</p>
-          </a>
-
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
           </a>
         </div>
       </main>
